7fa40f5208e99d5921a144729779581096137499
Joseph <joseph_m_allen@msn.com>
2022-01-06 11:16:41 -0500

    Update encrypt.sh

--- sh/encrypt.sh
+++ sh/encrypt.sh
@@ -3,10 +3,10 @@
 @echo off
 
 IF "%JAVA_HOME%" == "" (
-    echo To run encrypt.bat, set the JAVA_HOME environment variable to the directory where the Java Runtime Environment ^(JRE^) is installed.
+    echo -e "To run encrypt.bat, set the JAVA_HOME environment variable to the directory where the Java Runtime Environment \(JRE\) is installed."
 ) ELSE (
     IF NOT EXIST "%JAVA_HOME%" (
-        echo We couldn't find the Java Runtime Environment ^(JRE^) in directory "%JAVA_HOME%". To run process.bat, set the JAVA_HOME environment variable to the directory where the JRE is installed.
+        echo -e "We couldn't find the Java Runtime Environment \(JRE\) in directory "%JAVA_HOME%". To run process.bat, set the JAVA_HOME environment variable to the directory where the JRE is installed."
     ) ELSE (
         "%JAVA_HOME%\bin\java"  -cp ..\dataloader-51.0.1-uber.jar com.salesforce.dataloader.security.EncryptionUtil %*
     )
